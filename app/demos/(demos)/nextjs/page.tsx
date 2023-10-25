6bc4b2b4d015e1214bbc543a1255ee3a718ed35e
Dev Agrawal <dev@clerk.dev>
2023-10-25 12:58:47 -0700

    cleared nextjs demo

--- app/demos/(demos)/nextjs/page.tsx
+++ app/demos/(demos)/nextjs/page.tsx
@@ -1,9 +1,5 @@
 import { NextjsDemoC } from "@/components/nextjs-demo-c";
-import { auth } from "@clerk/nextjs";
-import { getTodos } from "./db";
 
 export default async function NextJsDemo() {
-  const { userId } = auth();
-  const todos = await getTodos(userId!);
-  return <NextjsDemoC title={`Clerk App Router`} todos={todos} />;
+  return <NextjsDemoC title={`Clerk App Router`} todos={[]} />;
 }
