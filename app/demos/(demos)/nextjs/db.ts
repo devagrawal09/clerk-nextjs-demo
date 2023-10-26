4061d31393eaf555d070852731607243634cdb5d
Dev Agrawal <dev@clerk.dev>
2023-10-26 09:46:01 -0700

    changed ids

--- app/demos/(demos)/nextjs/db.ts
+++ app/demos/(demos)/nextjs/db.ts
@@ -1,22 +1,22 @@
 const FakeDb = [
   {
     id: 1,
-    userId: "user_2XDStfItbr3wEQWK5Sgm3hK3mxz",
+    userId: "user_2XDT1UlgdlQdzUjaIqNoKr1Avkn",
     title: "Learn Next.js",
   },
   {
     id: 2,
-    userId: "user_2XDStfItbr3wEQWK5Sgm3hK3mxz",
+    userId: "user_2XDT1UlgdlQdzUjaIqNoKr1Avkn",
     title: "Deploy to Vercel",
   },
   {
     id: 3,
-    userId: "user_2XDStfItbr3wEQWK5Sgm3hK3mxz",
+    userId: "user_2XDT1UlgdlQdzUjaIqNoKr1Avkn",
     title: "Integrate Clerk",
   },
   {
     id: 4,
-    userId: "user_2XDStfItbr3wEQWK5Sgm3hK3mxz",
+    userId: "user_2XDT1UlgdlQdzUjaIqNoKr1Avkn",
     title: "Profit",
   },
   {
